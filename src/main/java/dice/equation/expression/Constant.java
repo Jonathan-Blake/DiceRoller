4345c57f1a4b85b1534e3fdeda9363606da940ac
Jonathan Blake <Jonathanblake46@outlook.com>
2021-08-05 12:17:06 +0100

    reformat

--- src/main/java/dice/equation/expression/Constant.java
+++ src/main/java/dice/equation/expression/Constant.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Constant {
     public static final String SPACE = " ";
     public static final Random RANDOM = new Random();
+
     private Constant() {
     }
 }
